fa79cc75368c25844d89079c54a4c728bd1ce47e
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2025-09-22 16:32:29 +0100

    solutions to: closures, hoisting and findFactorial

--- src/05_logic_control_flow/findFactorial.js
+++ src/05_logic_control_flow/findFactorial.js
@@ -6,8 +6,46 @@
 // The factorial of 2 is 2 * 1 = 2.
 // The factorial of 3 is 3 * 2 * 1 = 6.
 // Recommended: use a while of for loop to solve the problem
+
+// Sean Solution
+// Note: This works correctly, but starting factorial at 1 (as in the Senior Dev solution)
+// is more conventional, avoids the special-case check for 0, and makes the code easier
+// to read and maintain.
+
+// function findFactorial(n) {
+//   let factorial = n;
+
+//   if (n === 0) return 1;
+
+//   while (n > 1) {
+//     factorial = factorial * (n - 1);
+//     n--;
+//   }
+
+//   return factorial;
+// }
+
+// Senior Dev Solution
+// function findFactorial(n) {
+//   if (n === 0) {
+//     return 1;
+//   }
+//   let factorial = 1;
+//   while (n > 0) {
+//     factorial *= n;
+//     n--;
+//   }
+//   return factorial;
+// }
+
+// Alternative Solution:
 function findFactorial(n) {
-    // Your code here
+  let factorial = 1;
+  while (n > 0) {
+    factorial *= n;
+    n--;
+  }
+  return factorial;
 }
 
 module.exports = findFactorial;
